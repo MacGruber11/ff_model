78dc34ee0031f415e8ea9b03c759715d8f87b891
vladimir.stepanov <vladimir.stepanov@forforce.com>
2016-08-11 11:42:17 +0300

    first commit

---
+++ src/abilities/ModelAbility.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stepanov
+ * Date: 11.08.2016
+ * Time: 10:43
+ */
+namespace MacGruber11\ff_model\abilities;
+
+
+abstract class ffModelAbility
+{
+
+}
---
+++ src/ff_modelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MacGruber11\ff_model;
+
+use Illuminate\Support\ServiceProvider;
+
+class ff_modelServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
